a530f2e50d833680a0a5c8ad6285f484143fcde7
akshanabi <akshanabi913@gmail.com>
2024-12-05 16:07:37 +0530

    Add files via upload

---
+++ JS/add.js
@@ -0,0 +1,24 @@
+//let a = 5;
+//let b = 5;
+
+//console.log(a+b);
+
+let a = 10;
+let b = '10';
+if(a === b){
+    console.log("Welcome");
+    } else{
+     console.log("Not Allow");
+    }
+
+    let age = 21;
+    if(age > 18)
+        {
+        console.log("Right age to vote");
+    }else if(age <= 18)
+    {
+        console.log("Not right age to vote");
+    }else
+    {
+        console.log("age is not defined");
+    }
---
+++ JS/index.js
@@ -0,0 +1,26 @@
+let a="JS";
+
+let num = 123;
+
+let b;
+
+console.log(typeof(num));
+
+//console.log(string)
+//var name ="SHANTHI"
+
+let name="Javascipt"
+console.log(name);
+
+{
+    let another = name
+    console.log(another);
+
+   var lan = 123;
+
+}
+
+let anotherlan = lan
+console.log(anotherlan);
+
+//const lan="react"
